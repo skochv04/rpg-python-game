45029d6940d2f47c1d88a9e0c90032f793d2d84d
Dominik393 <Dominiksmykow@wp.pl>
2024-03-19 15:35:42 +0100

    Window Class Init

---
+++ Window.py
@@ -0,0 +1,20 @@
+import pygame
+
+pygame.init()
+
+class Window:
+    def __init__(self, width, height):
+        self.window = pygame.display.set_mode((width, height))
+        pygame.display.set_caption("FF RPG")
+
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+
+            # Fill the window with white color
+            self.window.fill((255, 255, 255))
+
+            # Update the display
+            pygame.display.update()
---
+++ main.py
@@ -0,0 +1,7 @@
+from Window import Window
+import pygame
+
+pygame.init()
+
+gra = Window(1280, 720)
+gra.run()
